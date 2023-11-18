9510eea809840945b56bc2bb7bd2d31e9b0c500e
Uzair <uzairzenkoders@gmail.com>
2023-11-18 19:57:49 +0500

    18-Nov-23

---
+++ test13.js
@@ -0,0 +1,23 @@
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+// For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+// The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+function twoSum(numbers, target) {
+    let indexes = []
+    for(let i = 0;i<numbers.length;i++){
+        let showInndexes = []
+        for(let j=0;j<numbers.length;j++){
+            if (i!=j && numbers[i]+numbers[j]===target){
+                showInndexes.push(i,j)
+            }
+        }
+        showCheck = showInndexes[0]===
+        showInndexes.length!=0 && indexes.push(showInndexes)
+    }return indexes[0]
+    // return indexes {for all the indexes}
+
+}
+
+console.log(twoSum([1, 2, 3], 4))
